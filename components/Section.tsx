15c0ee66fd427c16826431c36ad8887520205c9c
carstendold1@gmail.com <88604752+OriginalUncompiled@users.noreply.github.com>
2024-10-06 12:52:53 +0200

    Added show/hide buttons to sections

--- components/Section.tsx
+++ components/Section.tsx
@@ -1,4 +1,8 @@
-import React, { ReactNode } from 'react'
+'use client'
+
+import { cn } from '@/lib/utils'
+import { Triangle } from 'lucide-react'
+import React, { ReactNode, useState } from 'react'
 
 type SectionProps = {
   caption: string,
@@ -6,10 +10,24 @@ type SectionProps = {
 }
 
 const Section = ({ caption, children }: SectionProps) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(true);
+
+  const openCloseDropdown = (): void => {
+    setIsDropdownOpen((prev) => !prev);
+  }
+
   return (
-    <section className='bg-zinc-700/15 border border-neutral-800 rounded-lg p-4 pt-2 m-2 w-[95%] max-w-[550px]'>
-      <h2 className='text-center font-extrabold underline mb-2'>{caption}</h2>
-      {children}
+    <section className='bg-zinc-700/15 border border-neutral-800 rounded-lg p-4 py-2 m-2 w-[95%] max-w-[550px]'>
+      <div onClick={() => openCloseDropdown()} className='flex flex-row justify-center items-center cursor-pointer'>
+        <Triangle
+          size={10}
+          strokeWidth={3}
+          fill='white'
+          className={cn('mr-2', !isDropdownOpen && 'rotate-180')}
+        />
+        <h2 className='text-center font-extrabold underline'>{caption}</h2>
+      </div>
+      {isDropdownOpen ? children : <></>}
     </section>
   )
 }
